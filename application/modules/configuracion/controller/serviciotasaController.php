b4adab490bd0f2fbae7f0f855fe8076585512bb7
diemarc <diemarc.os@gmail.com>
2018-03-19 17:38:51 +0100

    Merge origin/development_dmrc into development_dmrc
    Conflicts:
    	.gitignore
    	nbproject/project.properties
    	nbproject/project.xml

--- application/modules/configuracion/controller/serviciotasaController.php
+++ application/modules/configuracion/controller/serviciotasaController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace application\modules\configuracion\controller;
+
 defined('__APPFOLDER__') OR exit('Direct access to this file is forbidden, siya');
 /*
   |-----------------------------------------------------------------------------
@@ -13,16 +14,15 @@ defined('__APPFOLDER__') OR exit('Direct access to this file is forbidden, siya'
   |
  */
 
-class ServiciotasaController extends \kerana\Kerana implements \kerana\KeranaInterface {
+class ServiciotasaController extends \kerana\Kerana implements \kerana\KeranaInterface
+{
 
-    
     protected $_serviciotasa;
 
-     public function __construct()
+    public function __construct()
     {
         parent::__construct();
-         $this->_serviciotasa= New \application\modules\configuracion\model\ServiciotasaModel();
-        
+        $this->_serviciotasa = New \application\modules\configuracion\model\ServiciotasaModel();
     }
 
     /**
@@ -30,90 +30,88 @@ class ServiciotasaController extends \kerana\Kerana implements \kerana\KeranaInt
      * Show all 
      * -------------------------------------------------------------------------
      */
-    public function index(){
-        
+    public function index()
+    {
+
         // only necesary for a view creator, remove it  after index files is
         // created
         \kerana\View::$model = $this->_serviciotasa;
-        \kerana\View::showView($this->_current_module, 'serviciotasa/index', 
-                ['rsServiciotasas' => $this->_serviciotasa->getAll()]);
+        \kerana\View::showView($this->_current_module, 'serviciotasa/index', ['rsServiciotasas' => $this->_serviciotasa->getAll()]);
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Add new
      * -------------------------------------------------------------------------
      */
-    
-    public function add(){
-         \kerana\View::$model = $this->_serviciotasa;
+    public function add()
+    {
+        \kerana\View::$model = $this->_serviciotasa;
         $params = [
- "rsServicios"=> $this->_serviciotasa->objServicioModel->getAll(), 
-
- "rsTaxas"=> $this->_serviciotasa->objTaxaModel->getAll(), 
-];
-        \kerana\View::showForm($this->_current_module,'serviciotasa/add',$params,$this->_serviciotasa);
+            "rsServicios" => $this->_serviciotasa->objServicioModel->getAll(),
+            "rsTaxas" => $this->_serviciotasa->objTaxaModel->getAll(),
+        ];
+        \kerana\View::showForm($this->_current_module, 'serviciotasa/add', $params, $this->_serviciotasa);
     }
-    
-     /**
+
+    /**
      * -------------------------------------------------------------------------
      * Save new record
      * -------------------------------------------------------------------------
      */
-    
-    public function save(){
+    public function save()
+    {
         ($this->_serviciotasa->savePost()) ? \helpers\Redirect::to('/configuracion/serviciotasa/index') : '';
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Show one record detail
      * -------------------------------------------------------------------------
      * @param int $id
      */
-    
-    public function detail($id){
-        
+    public function detail($id)
+    {
+
         $this->_serviciotasa->_setIdTableValue($id);
         $params['rsServiciotasa'] = $this->_serviciotasa->getRecord();
-        \kerana\View::showView($this->_current_module,'serviciotasa/detail',$params);
-        
+        \kerana\View::showView($this->_current_module, 'serviciotasa/detail', $params);
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Edit one record
      * -------------------------------------------------------------------------
      * @param int $id
      */
-    
-    public function edit($id){
+    public function edit($id)
+    {
         $this->_serviciotasa->_setIdTableValue($id);
-         \kerana\View::$model = $this->_serviciotasa;
+        \kerana\View::$model = $this->_serviciotasa;
         $params['rs'] = $this->_serviciotasa->getRecord();
-        \kerana\View::showForm($this->_current_module,'serviciotasa/edit',$params);
+        \kerana\View::showForm($this->_current_module, 'serviciotasa/edit', $params);
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Update one record
      * -------------------------------------------------------------------------
      * @param int $id
      */
-    
-    public function update($id){
+    public function update($id)
+    {
         $this->_serviciotasa->_setIdTableValue($id);
         ($this->_serviciotasa->savePost()) ? \helpers\Redirect::to('/configuracion/serviciotasa/index') : '';
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Delete one record
      * -------------------------------------------------------------------------
      * @param int $id
      */
-    
-    public function delete($id){
+    public function delete($id)
+    {
         $this->_serviciotasa->_setIdTableValue($id);
         ($this->_serviciotasa->delete()) ? \helpers\Redirect::to('/configuracion/serviciotasa/index') : '';
     }
