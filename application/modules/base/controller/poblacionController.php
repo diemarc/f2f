7a5aa90c284764ca1f28a20147eeb236ba72b640
diemarc <diemarc.os@gmail.com>
2018-03-20 06:46:10 +0100

    poblacion json searcj functionality

--- application/modules/base/controller/poblacionController.php
+++ application/modules/base/controller/poblacionController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace application\modules\base\controller;
+
 defined('__APPFOLDER__') OR exit('Direct access to this file is forbidden, siya');
 /*
   |-----------------------------------------------------------------------------
@@ -13,16 +14,15 @@ defined('__APPFOLDER__') OR exit('Direct access to this file is forbidden, siya'
   |
  */
 
-class PoblacionController extends \kerana\Kerana implements \kerana\KeranaInterface {
+class PoblacionController extends \kerana\Kerana implements \kerana\KeranaInterface
+{
 
-    
     protected $_poblacion;
 
-     public function __construct()
+    public function __construct()
     {
         parent::__construct();
-         $this->_poblacion= New \application\modules\base\model\PoblacionModel();
-        
+        $this->_poblacion = New \application\modules\base\model\PoblacionModel();
     }
 
     /**
@@ -30,88 +30,105 @@ class PoblacionController extends \kerana\Kerana implements \kerana\KeranaInterf
      * Show all 
      * -------------------------------------------------------------------------
      */
-    public function index(){
-        
+    public function index()
+    {
+
         // only necesary for a view creator, remove it  after index files is
         // created
         \kerana\View::$model = $this->_poblacion;
-        \kerana\View::showView($this->_current_module, 'poblacion/index', 
-                ['rsPoblacions' => $this->_poblacion->getAll()]);
+        \kerana\View::showView($this->_current_module, 'poblacion/index', ['rsPoblacions' => $this->_poblacion->getAll()]);
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Add new
      * -------------------------------------------------------------------------
      */
-    
-    public function add(){
-         \kerana\View::$model = $this->_poblacion;
+    public function add()
+    {
+        \kerana\View::$model = $this->_poblacion;
         $params = [];
-        \kerana\View::showForm($this->_current_module,'poblacion/add',$params,$this->_poblacion);
+        \kerana\View::showForm($this->_current_module, 'poblacion/add', $params, $this->_poblacion);
     }
-    
-     /**
+
+    /**
      * -------------------------------------------------------------------------
      * Save new record
      * -------------------------------------------------------------------------
      */
-    
-    public function save(){
+    public function save()
+    {
         ($this->_poblacion->savePost()) ? \helpers\Redirect::to('/base/poblacion/index') : '';
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Show one record detail
      * -------------------------------------------------------------------------
      * @param int $id
      */
-    
-    public function detail($id){
-        
+    public function detail($id)
+    {
+
         $this->_poblacion->_setIdTableValue($id);
         $params['rsPoblacion'] = $this->_poblacion->getRecord();
-        \kerana\View::showView($this->_current_module,'poblacion/detail',$params);
-        
+        \kerana\View::showView($this->_current_module, 'poblacion/detail', $params);
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Edit one record
      * -------------------------------------------------------------------------
      * @param int $id
      */
-    
-    public function edit($id){
+    public function edit($id)
+    {
         $this->_poblacion->_setIdTableValue($id);
-         \kerana\View::$model = $this->_poblacion;
+        \kerana\View::$model = $this->_poblacion;
         $params['rs'] = $this->_poblacion->getRecord();
-        \kerana\View::showForm($this->_current_module,'poblacion/edit',$params);
+        \kerana\View::showForm($this->_current_module, 'poblacion/edit', $params);
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Update one record
      * -------------------------------------------------------------------------
      * @param int $id
      */
-    
-    public function update($id){
+    public function update($id)
+    {
         $this->_poblacion->_setIdTableValue($id);
         ($this->_poblacion->savePost()) ? \helpers\Redirect::to('/base/poblacion/index') : '';
     }
-    
+
     /**
      * -------------------------------------------------------------------------
      * Delete one record
      * -------------------------------------------------------------------------
      * @param int $id
      */
-    
-    public function delete($id){
+    public function delete($id)
+    {
         $this->_poblacion->_setIdTableValue($id);
         ($this->_poblacion->delete()) ? \helpers\Redirect::to('/base/poblacion/index') : '';
     }
 
+    /*
+      |--------------------------------------------------------------------------
+      | JSONDATA
+      |--------------------------------------------------------------------------
+      |
+     */
+    
+    /**
+     * -------------------------------------------------------------------------
+     * Find poblacion and return json data
+     * -------------------------------------------------------------------------
+     * @param type $poblacion
+     * @return type
+     */
+    public function findPoblacion($poblacion){
+        return $this->_poblacion->searchPoblacionJson($poblacion);
+    }
+    
 }
