62622733d5ad8564f19173eeea3ea168e2c42952
diemarc <diemarc.os@gmail.com>
2018-03-20 06:45:29 +0100

    ADA json return implementation.

--- public/_layouts/f2f/_htmlHeader.php
+++ public/_layouts/f2f/_htmlHeader.php
@@ -16,9 +16,8 @@
         <link rel="stylesheet" href="/_layouts/f2f/dist/css/AdminLTE.min.css">
         <!-- AdminLTE Skins. Choose a skin from the css/skins
              folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="/_layouts/f2f/dist/css/skins/_all-skins.min.css">
+        <link rel="stylesheet" href="/_layouts/f2f/dist/css/skins/skin-blue.min.css">
         <!-- Morris chart -->
-        <link rel="stylesheet" href="/_layouts/f2f/bower_components/morris.js/morris.css">
         <!-- jvectormap -->
         <link rel="stylesheet" href="/_layouts/f2f/bower_components/jvectormap/jquery-jvectormap.css">
         <!-- Date Picker -->
