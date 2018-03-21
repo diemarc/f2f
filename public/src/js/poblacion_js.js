73f139a87c8a487a76e93e201b6652e99d24c35e
diemarc <diemarc.os@gmail.com>
2018-03-21 06:08:14 +0100

    poblacion controller

---
+++ public/src/js/poblacion_js.js
@@ -0,0 +1,34 @@
+
+
+/**
+ * -----------------------------------------------------------------------------
+ * Live search localidades/poblacion
+ * -----------------------------------------------------------------------------
+ * @param {type} localidad
+ * @returns {undefined}
+ */
+function findLocalidad(localidad) {
+
+
+    $('#live_search_poblacion').removeClass('hidden');
+    var url = '/base/poblacion/findPoblacion/' + localidad;
+    $.getJSON(url, function (data) {
+        console.log(data);
+
+        var poblacion_output = "";
+        $.each(data.poblaciones, function (i, poblacion) {
+            poblacion_output += "<tr onclick='asignarLocalidad(\"" + poblacion.poblacion + "\",\"" + poblacion.id_poblacion + "\")'>"
+                    + "<td>" + poblacion.poblacion + " (" + poblacion.provincia + ") </td>"
+                    + "</tr>";
+            $('#json_localidades').html(poblacion_output);
+        });
+    });
+}
+
+
+function asignarLocalidad(localidad, id_localidad) {
+    $('#live_search_poblacion').addClass('hidden');
+    $('#f_id_poblacion').val(id_localidad);
+    $('#k_localidad').val(localidad);
+
+}
