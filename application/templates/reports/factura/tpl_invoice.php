197c0bdd5cad356c1ec4e28b7599e844d9c5662d
diemarc <diemarc.os@gmail.com>
2018-04-24 17:30:03 +0200

    Pdf helper

--- application/templates/reports/factura/tpl_invoice.php
+++ application/templates/reports/factura/tpl_invoice.php
@@ -1,9 +1,9 @@
 <style type="text/css">
-<!--
-table { vertical-align: top; }
-tr    { vertical-align: top; }
-td    { vertical-align: top; }
--->
+    <!--
+    table { vertical-align: top; }
+    tr    { vertical-align: top; }
+    td    { vertical-align: top; }
+    -->
 </style>
 <page backcolor="#FEFEFE" backimgx="center" 
       backimgy="bottom" backimgw="100%" backtop="10mm" 
@@ -13,9 +13,11 @@ td    { vertical-align: top; }
     <table cellspacing="0" style="width: 100%; text-align: center; font-size: 14px">
         <tr>
             <td style="width: 75%;">
+
+
             </td>
             <td style="width: 25%; color: #444444;">
-                <img style="width: 100%;" src="<?php echo __URL__.'/data/logos/seva/ipr_small.jpg';?>"
+                <img style="width: 100%;" src="<?php echo __URL__ . '/data/logos/seva/ipr_small.jpg'; ?>"
                      alt="Logo"><br>
                 RELATION CLIENT
             </td>
@@ -25,12 +27,17 @@ td    { vertical-align: top; }
     <br>
     <table cellspacing="0" style="width: 100%; text-align: left; font-size: 11pt;">
         <tr>
-            <td style="width:50%;"></td>
+            <td style="width:50%;">contratante</td>
             <td style="width:14%; ">Client :</td>
             <td style="width:36%">M. Albert Dupont</td>
         </tr>
         <tr>
-            <td style="width:50%;"></td>
+            <td style="width:50%;">cif contratante</td>
+            <td style="width:14%; ">CIF :</td>
+            <td style="width:36%">sdsd</td>
+        </tr>
+        <tr>
+            <td style="width:50%;">direccion contratante</td>
             <td style="width:14%; ">Adresse :</td>
             <td style="width:36%">
                 Résidence perdue<br>
@@ -39,36 +46,32 @@ td    { vertical-align: top; }
             </td>
         </tr>
         <tr>
-            <td style="width:50%;"></td>
+            <td style="width:50%;">Email contratante</td>
             <td style="width:14%; ">Email :</td>
             <td style="width:36%">nomail@domain.com</td>
         </tr>
         <tr>
-            <td style="width:50%;"></td>
+            <td style="width:50%;">telefono contratante</td>
             <td style="width:14%; ">Tel :</td>
             <td style="width:36%">33 (0) 1 00 00 00 00</td>
         </tr>
     </table>
     <br>
     <br>
-    <table cellspacing="0" style="width: 100%; text-align: left;font-size: 10pt">
-        <tr>
-            <td style="width:50%;"></td>
-            <td style="width:50%; ">Spipu Ville, le <?php echo date('d/m/Y'); ?></td>
-        </tr>
-    </table>
+   
     <br>
     <i>
         <b><u>Objet </u>: &laquo; Bon de Retour &raquo;</b><br>
-        Compte client : <?php echo $rsFactura->num_factura;?><br>
-        Factura : <?php echo $rsFactura->num_factura;?><br>
+        Compte client : <?php echo $rsFactura->num_factura; ?><br>
+        Factura : <?php echo $rsFactura->num_factura; ?><br>
     </i>
     <br>
     <br>
     Madame, Monsieur, Cher Client,<br>
     <br>
     <br>
-    Nous souhaitons vous informer que le dossier <b>71326</b> concernant un &laquo; Bon de Retour &raquo; pour les articles suivants a été accepté.<br>
+    Nous souhaitons vous informer que le dossier <b>71326</b> concernant un &laquo; Bon de Retour &raquo; 
+    pour les articles suivants a été accepté.<br>
     <br>
     <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #E7E7E7; text-align: center; font-size: 10pt;">
         <tr>
@@ -79,30 +82,30 @@ td    { vertical-align: top; }
             <th style="width: 13%">Prix Net</th>
         </tr>
     </table>
-<?php
+    <?php
     $nb = rand(5, 11);
     $produits = array();
     $total = 0;
-    for ($k=0; $k<$nb; $k++) {
+    for ($k = 0; $k < $nb; $k++) {
         $num = rand(100000, 999999);
-        $nom = "le produit n°".rand(1, 100);
+        $nom = "le produit n°" . rand(1, 100);
         $qua = rand(1, 20);
-        $prix = rand(100, 9999)/100.;
-        $total+= $prix*$qua;
+        $prix = rand(100, 9999) / 100.;
+        $total+= $prix * $qua;
         $produits[] = array($num, $nom, $qua, $prix, rand(0, $qua));
-?>
-    <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #F7F7F7; text-align: center; font-size: 10pt;">
-        <tr>
-            <td style="width: 12%; text-align: left"><?php echo $num; ?></td>
-            <td style="width: 52%; text-align: left"><?php echo $nom; ?></td>
-            <td style="width: 13%; text-align: right"><?php echo number_format($prix, 2, ',', ' '); ?> &euro;</td>
-            <td style="width: 10%"><?php echo $qua; ?></td>
-            <td style="width: 13%; text-align: right;"><?php echo number_format($prix*$qua, 2, ',', ' '); ?> &euro;</td>
-        </tr>
-    </table>
-<?php
+        ?>
+        <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #F7F7F7; text-align: center; font-size: 10pt;">
+            <tr>
+                <td style="width: 12%; text-align: left"><?php echo $num; ?></td>
+                <td style="width: 52%; text-align: left"><?php echo $nom; ?></td>
+                <td style="width: 13%; text-align: right"><?php echo number_format($prix, 2, ',', ' '); ?> &euro;</td>
+                <td style="width: 10%"><?php echo $qua; ?></td>
+                <td style="width: 13%; text-align: right;"><?php echo number_format($prix * $qua, 2, ',', ' '); ?> &euro;</td>
+            </tr>
+        </table>
+        <?php
     }
-?>
+    ?>
     <table cellspacing="0" style="width: 100%; border: solid 1px black; background: #E7E7E7; text-align: center; font-size: 10pt;">
         <tr>
             <th style="width: 87%; text-align: right;">Total : </th>
