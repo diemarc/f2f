b7f124169818ff8180c42da8b800d2bfd3311196
diemarc <diemarc.os@gmail.com>
2018-04-24 06:10:28 +0200

    Pdf helper

---
+++ application/modules/fac2fast/controller/f2fInformesController.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This file is part of keranaProject
+ * Copyright (C) 2017-2018  diemarc  diemarc@protonmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace application\modules\fac2fast\controller;
+
+defined('__APPFOLDER__') OR exit('Direct access to this file is forbidden, siya');
+
+/**
+ * -----------------------------------------------------------------------------
+ * f2fInformesController
+ * ------------------------------------------------------------------------------
+ * @author diemarc
+ */
+class F2fInformesController extends \kerana\Kerana
+{
+    
+    protected
+            
+            /** @object, f2f report model */
+            $f2fReport_model; 
+    
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->f2fReport_model = new \application\modules\fac2fast\model\F2fReportModel();
+    }
+    
+    /**
+     * -------------------------------------------------------------------------
+     * Genera una factura
+     * -------------------------------------------------------------------------
+     * @param type $id
+     * @return type
+     */
+    public function generarFactura($id){
+        
+        return $this->f2fReport_model->parseFactura($id);
+        
+    }
+    
+    
+}
