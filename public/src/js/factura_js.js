cbafc6743da0f03d8b0881e097b3298b32fc5638
diemarc <diemarc.os@gmail.com>
2018-04-26 10:22:35 +0200

    neuva factura

---
+++ public/src/js/factura_js.js
@@ -0,0 +1,36 @@
+
+/**
+ * -----------------------------------------------------------------------------
+ * Submit a new empresa from factura an run callback to get
+ * the new empresa created
+ * -----------------------------------------------------------------------------
+ * @returns {undefined}
+ */
+function saveEmpresaFactura() {
+
+
+    $.ajax({
+        type: "POST",
+        url: $("#formNewEmpresa").attr('action'),
+        data: $("#formNewEmpresa").serialize(), // Adjuntar los campos del formulario enviado.
+        success: function (data)
+        {
+           var json = $.parseJSON(data);
+           // if is success
+           if(json.success){
+               asignarEmpresa(json.cliente,json.id_empresa);
+               $('#div_cliente_notfound').addClass('hidden');
+               $('#myModel').modal('hide');
+           }else{
+               alert('un error ocurrio');
+           }
+            
+        },
+        error: function (data) {
+            alert('An Error occured, see the console.log');
+            console.log('An error occurred.');
+            console.log(data);
+        }
+    });
+
+}
