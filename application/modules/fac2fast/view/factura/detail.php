ff07e02d956bc7ce3612acc9ee806dc26dbc1660
diemarc <diemarc.os@gmail.com>
2018-05-09 07:10:46 +0200

    support for getRecord to return array, multiple objects like a rs.

--- application/modules/fac2fast/view/factura/detail.php
+++ application/modules/fac2fast/view/factura/detail.php
@@ -8,88 +8,156 @@
 
     </section> 
     <!-- Main content -->
-    <section class="content">
-        <!-- Default box -->
-        <div class="box">
-            <div class="box-header with-border">
-                <h3 class="box-title text-blue">
-                    <strong><?php echo $rsFactura->num_factura; ?></strong>
-                </h3>
-            </div>
-            <div class="box-body">
-                <div class="row">
-                    <div class="col-lg-8">
-                        <div class="box box-info">
-                            <div class="box-header with-border">
-                                <h3 class="box-title">
-                                    <a href="<?php echo __URL__ . '/base/contratante/edit/' . $rsFactura->id_factura; ?>" 
-                                       class="btn btn-sm btn-app" title="Editar factura">
-                                        <i class="fa fa-edit text-primary"></i>
-                                        <span class="text-primary">Editar</span>
-                                    </a>
-                                    <a href="<?php echo __URL__ . '/base/contratante/edit/' . $rsFactura->id_factura; ?>" 
-                                       class="btn btn-sm btn-app" title="Descargar factura">
-                                        <i class="fa fa-file-pdf-o text-danger"></i>
-                                        <span class="text-danger">Descargar</span>
-                                    </a>
-                                    <a href="<?php echo __URL__ . '/base/contratante/edit/' . $rsFactura->id_factura; ?>" 
-                                       class="btn btn-sm btn-app" title="Enviar factura por email">
 
-                                        <i class="fa fa-envelope text-success"></i>
-                                        <span class="text-success">Enviar</span>
-                                    </a>
-                                </h3>
-                            </div>
-                            <!-- /.box-header -->
-                            <!-- form start -->
-                            <div class="box-body box-profile">
-                                <header class="breadcrumb">
-                                    Datos fiscales
-                                </header>
-                                <ul class="list-group list-group-unbordered">
-                                    <li class="list-group-item">
-                                        <b>Raz&oacute;n social</b> <a class="pull-right"><?php echo $rsFactura->razon_social; ?></a>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <b>CIF</b> <a class="pull-right"><?php echo $rsFactura->cif; ?></a>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <b>Direcci&oacute;n</b> <a class="pull-right"><?php echo $rsFactura->direccion; ?></a>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <b>Localidad</b> <a class="pull-right"><?php echo $rsFactura->poblacion . ' (' . $rsFactura->provincia . ')'; ?></a>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <b>Tel&eacute;fono</b> <a class="pull-right"><?php echo $rsFactura->telefono; ?></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="box box-info box-solid">
-                            <div class="box-header with-border">
-                                <h3 class="box-title">Otras facturas</h3>
-
-                                <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                                    </button>
-                                </div>
-                                <!-- /.box-tools -->
-                            </div>
-                            <!-- /.box-header -->
-                            <div class="box-body">
-                                The body of the box
-                            </div>
-                            <!-- /.box-body -->
-                        </div>
-                    </div>
+    <div class="col-sm-8">
+        <section class="content">
+            <!-- Default box -->
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title text-blue">
+                        <strong><?php echo $rsFactura->num_factura; ?></strong>
+                    </h3>
                 </div>
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="box">
+                                <div class="box-header with-border">
+                                    <div class="box-title">
+                                        <a href="<?php echo __URL__ . '/base/contratante/edit/' . $rsFactura->id_factura; ?>" 
+                                           class="btn btn-sm btn-app" title="Editar factura">
+                                            <i class="fa fa-edit text-primary"></i>
+                                            <span class="text-primary">Editar</span>
+                                        </a>
+                                        <a href="<?php echo __URL__ . '/base/contratante/edit/' . $rsFactura->id_factura; ?>" 
+                                           class="btn btn-sm btn-app" title="Descargar factura">
+                                            <i class="fa fa-file-pdf-o text-danger"></i>
+                                            <span class="text-danger">Descargar</span>
+                                        </a>
+                                        <a href="<?php echo __URL__ . '/base/contratante/edit/' . $rsFactura->id_factura; ?>" 
+                                           class="btn btn-sm btn-app" title="Enviar factura por email">
 
+                                            <i class="fa fa-envelope text-success"></i>
+                                            <span class="text-success">Enviar</span>
+                                        </a>
+                                    </div>
+                                    <div class="col-md-3 col-sm-6 col-xs-12 pull-right">
+                                        <div class="info-box">
+                                            <span class="info-box-icon">
+                                                <i class="fa fa-euro"></i></span>
+
+                                            <div class="info-box-content">
+                                                <span class="info-box-text">Total</span>
+                                                <span class="info-box-number">
+                                                    <?php echo $total;?>
+                                                </span>
+                                            </div>
+                                            <!-- /.info-box-content -->
+                                        </div>
+                                        <!-- /.info-box -->
+                                    </div>
+                                </div>
+                                <!-- /.box-header -->
+                                <!-- datos clientes -->
+                                <div class="box-body box-profile">
+                                    <header class="breadcrumb text-blue">
+                                        Datos de cliente facturado
+                                    </header>
+                                    <ul class="list-group list-group-unbordered">
+                                        <li class="list-group-item">
+                                            <b>Raz&oacute;n social</b> <a class="pull-right"><?php echo $rsFactura->razon_social; ?></a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <b>CIF</b> <a class="pull-right"><?php echo $rsFactura->cif; ?></a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <b>Direcci&oacute;n</b> <a class="pull-right"><?php echo $rsFactura->direccion; ?></a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <b>Localidad</b> <a class="pull-right"><?php echo $rsFactura->poblacion . ' (' . $rsFactura->provincia . ')'; ?></a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <b>Tel&eacute;fono</b> <a class="pull-right"><?php echo $rsFactura->telefono; ?></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!-- /.datos clientes -->
+                                <!-- servicios facturados -->
+                                <div class="box-body box-profile">
+                                    <header class="breadcrumb text-green">
+                                        Servicios facturados
+                                    </header>
+                                    <table class="table table-bordered 
+                                           table-striped table-condensed table-hover">
+                                        <thead>
+                                            <tr class="bg-success">
+                                                <th>Servicio</th>
+                                                <th style="">Base imponible</th>
+                                                <th style="">Cantidad</th>
+                                                <th style="">IVA</th>
+                                                <th style="">Retenci&oacute;n</th>
+                                                <th style="">Total</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php foreach ($rsFacturasServicios AS $servicio): ?>
+                                                <tr class="">
+                                                    <td>
+                                                        <?php echo $servicio->servicio; ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php echo $servicio->precio; ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php echo $servicio->cantidad; ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php echo $servicio->iva; ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php echo $servicio->retencion; ?>
+                                                    </td>
+                                                    <td style="">
+                                                        <?php echo $servicio->total; ?>
+                                                    </td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                        </tbody>
+
+                                    </table>
+
+
+                                </div>
+                                <!-- /.servicios facturados -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+
+        </section>
+    </div>
+    <div class="col-sm-4">
+        <section class="content">
+            <div class="box box-info box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Otras facturas</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    <!-- /.box-tools -->
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    The body of the box
+                </div>
+                <!-- /.box-body -->
             </div>
-            <!-- /.box-body -->
-        </div>
-        <!-- /.box -->
+        </section>
+    </div>
 
-    </section>
+
 </div>
