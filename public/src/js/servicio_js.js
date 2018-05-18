825eaefd950dece511c341b372c157073d3fcc03
diemarc <diemarc.os@gmail.com>
2018-05-18 07:05:50 +0200

    service js.

--- public/src/js/servicio_js.js
+++ public/src/js/servicio_js.js
@@ -67,70 +67,89 @@ function addService(id_service) {
 
             // checkbox
             data_service += '<td>';
-            data_service += '<input type="checkbox" name="f_concepto[' + data.record.id_servicio + ']" checked value="' + data.record.id_servicio + '"/>';
+            data_service += '<input type="checkbox" name="f_concepto[' + data.record.id_servicio + ']" \n\
+                checked value="' + data.record.id_servicio + '"/>';
             data_service += '</td>';
 
             // servicio
             data_service += '<td>';
-            data_service += '<div class="col-sm-12 text-primary">';
-            data_service += ' <strong><i class="fa fa-comment-o" title="Click para agregar detalles del servicio" ';
-            data_service += ' onclick="showObsServicio(' + data.record.id_servicio + ')"></i></strong> ';
+            data_service += '<div class="text-primary">';
+            data_service += ' <strong><i class="fa fa-comment-o" \n\
+            title="Click para agregar detalles del servicio" ';
+            data_service += ' onclick="showObsServicio(' + data.record.id_servicio + ')"></i>\n\
+            </strong> ';
             data_service += data.record.servicio;
             data_service += '</div>';
             
             // servicio personalizacion
             data_service += '<div class="col-sm-12 hidden" id="person_' + data.record.id_servicio + '"> ';
             data_service += '<div class="input-group has-success">';
-            data_service += '<div class="input-group-addon"><i class="fa fa-close" onclick="cleanObsServicio(' + data.record.id_servicio + ')"></i></div>';
+            data_service += '<div class="input-group-addon"><i class="fa fa-close" \n\
+                onclick="cleanObsServicio(' + data.record.id_servicio + ')"></i></div>';
             data_service += '<textarea class="form-control form-control-sm" rows="1" ';
             data_service += 'id="f_concepto_personalizacion_' + data.record.id_servicio + '"';
-            data_service += 'name="f_concepto_personalizacion[' + data.record.id_servicio + ']"></textarea>';
+            data_service += 'name="f_concepto_personalizacion[' + data.record.id_servicio + ']"\n\
+            ></textarea>';
             data_service += '</div>';
             data_service += '</div>';
             data_service += '</td>';
             
              // cantidades
-            data_service += '<td> <div class="col-xs-4"> ';
-            data_service += '<input type="number" class="form-control input-sm pull-right" onkeyup="changeCantidad('+data.record.id_servicio+')"';
+            data_service += '<td>';
+            data_service += '<input type="number" class="form-control input-sm pull-right" \n\
+                onkeyup="changeCantidad('+data.record.id_servicio+')"\n\
+                onchange="changeCantidad('+data.record.id_servicio+')"';
             data_service += 'name="f_concepto_cantidad[' + data.record.id_servicio + ']"';
             data_service += 'value="1" id="f_cantidad_' + data.record.id_servicio + '"/> ';
-            data_service += '</div></td> ';
+            data_service += '</td> ';
             
             // precio
-            data_service += '<td> <div class="col-xs-6"> ';
+            data_service += '<td>';
             data_service += '<div class="input-group">';
             data_service += '<div class="input-group-addon">';
             data_service += '<i class="fa fa-euro"></i>';
             data_service += '</div>';
-            data_service += '<input type="number" id="f_concepto_precio_'+data.record.id_servicio+'" class="form-control input-sm form-control-sm"';
+            data_service += '<input type="number" id="f_concepto_precio_'+data.record.id_servicio+'" \n\
+                class="form-control input-sm form-control-sm"';
             data_service += 'name="f_concepto_precio[' + data.record.id_servicio + ']"';
-            data_service += 'value="' + data.record.precio + '" onkeyup="changeCantidad('+data.record.id_servicio+')" /> </div>';
-            data_service += '</div></td> ';
+            data_service += 'value="' + data.record.precio + '" \n\
+                onkeyup="changeCantidad('+data.record.id_servicio+')"\n\
+                onkeyup="changeCantidad('+data.record.id_servicio+')" /> </div>';
+            data_service += '</td> ';
             
             // iva
-            data_service += '<td> <div class="col-sm-4"> ';
-            data_service += '<input type="number" step="0.01" class="form-control input-sm pull-right"';
-            data_service += 'name="f_concepto_iva[' + data.record.id_servicio + ']" onkeyup="changeCantidad('+data.record.id_servicio+')"';
-            data_service += 'value="' + data.record.iva_servicio + '" id="f_iva_' + data.record.id_servicio + '"/> ';
-            data_service += '</div></td> ';
+            data_service += '<td> ';
+            data_service += '<input type="number" step="0.01" class="form-control \n\
+                input-sm pull-right"';
+            data_service += 'name="f_concepto_iva[' + data.record.id_servicio + ']" \n\
+                onkeyup="changeCantidad('+data.record.id_servicio+')" \n\
+                onkeyup="changeCantidad('+data.record.id_servicio+')"';
+            data_service += 'value="' + data.record.iva_servicio + '" \n\
+                id="f_iva_' + data.record.id_servicio + '"/> ';
+            data_service += '</td> ';
 
             // retencion
-            data_service += '<td> <div class="col-xs-3 col-sm-4"> ';
-            data_service += '<input type="number" step="0.01" class="form-control input-sm pull-right"';
-            data_service += 'name="f_concepto_retencion[' + data.record.id_servicio + ']" onkeyup="changeCantidad('+data.record.id_servicio+')"';
-            data_service += 'value="' + data.record.retencion_servicio + '" id="f_retencion_' + data.record.id_servicio + '"/> ';
-            data_service += '</div></td> ';
+            data_service += '<td> ';
+            data_service += '<input type="number" step="0.01" class="form-control \n\
+                input-sm pull-right"';
+            data_service += 'name="f_concepto_retencion[' + data.record.id_servicio + ']" \n\
+                onkeyup="changeCantidad('+data.record.id_servicio+')"\n\
+                onkeyup="changeCantidad('+data.record.id_servicio+')"';
+            data_service += 'value="' + data.record.retencion_servicio + '" \n\
+                id="f_retencion_' + data.record.id_servicio + '"/> ';
+            data_service += '</td> ';
 
             // total
-            data_service += '<td> <div class="col-sm-6"> ';
+            data_service += '<td>';
             data_service += '<div class="input-group">';
             data_service += '<div class="input-group-addon">';
             data_service += '<span class="text-primary"><i class="fa fa-euro"></i></span>';
             data_service += '</div>';
-            data_service += '<input type="text" disabled step="0.01" class="form-control input-sm form-control-sm"';
+            data_service += '<input type="text" disabled step="0.01" \n\
+                class="form-control input-sm form-control-sm"';
             data_service += 'name="" id="total_' + data.record.id_servicio + '"';
             data_service += 'value="' + data.record.total_serv + '"/> </div>';
-            data_service += '</div></td> ';
+            data_service += '</td> ';
 
             // end tr
             data_service += '</tr>';
