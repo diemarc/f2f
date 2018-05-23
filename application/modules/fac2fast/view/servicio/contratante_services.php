2bc207a9221429afe2cb6a3b77acd0b6b1b9f247
diemarc <diemarc.os@gmail.com>
2018-05-23 05:15:28 +0200

    Correccion de autosuma de servicios de la factura.

--- application/modules/fac2fast/view/servicio/contratante_services.php
+++ application/modules/fac2fast/view/servicio/contratante_services.php
@@ -5,7 +5,7 @@
         loadListServices();
 
 // search html data
-        $("#filter_buscar_agrupacion").keyup(function () {
+        $("#filter_search_service").keyup(function () {
             var filter = $(this).val();
 
             $("table.table_services > tbody > tr").each(function () {
@@ -30,7 +30,7 @@
 <div class="box box-primary direct-chat direct-chat-primary">
     <div class="modal-header">
         <div class="box-header with-border">
-            <h2 class="box-title text-primary">Mis servicios</h2>
+            <h1 class="box-title text-primary">Mis servicios</h1>
 
             <div class="box-tools pull-right">
                 <button type="button" class="close" data-dismiss="modal"
@@ -40,48 +40,52 @@
         </div>
     </div>
     <div class="modal-body">
+        <div class="box-header">
+            <div class="well">
+                <div class='form-group form-group-sm'> 
+                    <div class='col-sm-8'> 
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="fa fa-search"></i>
+                            </div>
+                            <input type="text" class="form-control input-sm" 
+                                   id="filter_search_service" 
+                                   name="filter_search_service"
+                                   aria-describedby="inputSuccess3Status"
+                                   value="" placeholder="Busca un servicio, o crea uno nuevo"/>
+                        </div>
+                    </div> 
+                    <div class="col-sm-2">
+                        <button type="button" class="btn btn-primary btn-sm"
+                                data-toggle="modal" 
+                                data-remote="<?php echo __URL__ . '/fac2fast/servicio/add'; ?>" 
+                                data-target="#myModel"
+                                ><i class="fa fa-plus"></i> Nuevo</button>
+                    </div>
+                </div> 
+            </div> 
+        </div>
         <!-- /.box-header -->
         <div class="box-body">
             <div class="box">
-                <div class="box-header">
-                    <h3 class="box-title">
-
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-default btn-sm"
-                                    data-toggle="modal" 
-                                    data-remote="<?php echo __URL__ . '/fac2fast/servicio/add'; ?>" 
-                                    data-target="#myModel"
-                                    ><i class="fa fa-plus"></i> Nuevo</button>
-
-                        </div>
-
-                    </h3>
-                </div>
                 <!-- /.box-header -->
                 <div class="box-body no-padding">
-                    <input type="text" class="form-control input-sm" 
-                           id="filter_buscar_agrupacion" 
-                           name="filter_buscar_agrupacion"
-                           aria-describedby="inputSuccess3Status"
-                           value="">
-                    <span class="fa fa-search form-control-feedback" aria-hidden="true"></span>
-                    <span id="inputSuccess3Status" class="sr-only">(success)</span>  
-                    <table class="table_services table table-condensed table-hover">
-                        <thead>
-                            <tr class="bg-aqua-gradient">
-                                <th style="width: 10px">#</th>
-                                <th>Servicio</th>
-                                <th>Desc</th>
-                                <th>PrecioBase</th>
-                                <th>Iva</th>
-                                <th>Retenci&oacute;n</th>
-                                <th style="width: 40px"></th>
-                            </tr>
-                        </thead>
-                        <tbody id="div_services_list">
 
-                        </tbody>
-                    </table>
+                    <div style="height: 500px">
+                        <table class="table_services table table-condensed table-hover">
+                            <thead>
+                                <tr class="bg-aqua-gradient">
+                                    <th style="width: 10px">#</th>
+                                    <th>Servicio</th>
+                                    <th>Desc</th>
+                                    <th style="width: 40px"></th>
+                                </tr>
+                            </thead>
+                            <tbody id="div_services_list">
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 <!-- /.box-body -->
             </div>
