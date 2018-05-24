5fb7e6eba0a894c8bc6ac31d4fe226309ab39474
diemarc <diemarc.os@gmail.com>
2018-05-24 06:08:47 +0200

    view top refactoring.

--- application/modules/base/view/contratante/index.php
+++ application/modules/base/view/contratante/index.php
@@ -1,76 +1,94 @@
-<div id="page-wrapper">
-    <h2>base/contratante/index</h2>
-    <header class="breadcrumb">
-        <a href="http://local.factufacil/base/contratante/add" 
-           class="btn btn-success">Add new</a>
-    </header>
-    <section id='results' class='table-responsive'>
-      <table class="table table-bordered table-condensed table-hover">
-        <thead class='bg-warning'>
-            <tr>
-                <th>id_contratante</th> 
-<th>contratante</th> 
-<th>cif</th> 
-<th>razon_social</th> 
-<th>id_poblacion</th> 
-<th>direccion</th> 
-<th>telefono</th> 
-<th>email</th> 
-<th>contacto</th> 
-<th>cta_bancaria</th> 
-<th>path_logo</th> 
-<th>observacion</th> 
-<th>created_at</th> 
-<th>created_by</th> 
-<th>aux_estados_id_estado</th> 
-<th>estado</th> 
-<th>poblacion</th> 
-<th>provincia</th> 
-<th>ccaa</th> 
-<th>cod_provincia</th> 
-<th>cod_ccaa</th> 
-<th>cod_poblacion</th> 
-<th>Options</th> 
+<div class="content-wrapper">
+    <section class="content-header">
+        <h1>
+            <i class="fa fa-wrench fa-2x"></i> Configuraci&oacute;n f2F
 
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($rsContratantes AS $contratante):?>
-<tr> 
-<td><?php echo $contratante->id_contratante; ?></td> 
-<td><?php echo $contratante->contratante; ?></td> 
-<td><?php echo $contratante->cif; ?></td> 
-<td><?php echo $contratante->razon_social; ?></td> 
-<td><?php echo $contratante->id_poblacion; ?></td> 
-<td><?php echo $contratante->direccion; ?></td> 
-<td><?php echo $contratante->telefono; ?></td> 
-<td><?php echo $contratante->email; ?></td> 
-<td><?php echo $contratante->contacto; ?></td> 
-<td><?php echo $contratante->cta_bancaria; ?></td> 
-<td><?php echo $contratante->path_logo; ?></td> 
-<td><?php echo $contratante->observacion; ?></td> 
-<td><?php echo $contratante->created_at; ?></td> 
-<td><?php echo $contratante->created_by; ?></td> 
-<td><?php echo $contratante->aux_estados_id_estado; ?></td> 
-<td><?php echo $contratante->estado; ?></td> 
-<td><?php echo $contratante->poblacion; ?></td> 
-<td><?php echo $contratante->provincia; ?></td> 
-<td><?php echo $contratante->ccaa; ?></td> 
-<td><?php echo $contratante->cod_provincia; ?></td> 
-<td><?php echo $contratante->cod_ccaa; ?></td> 
-<td><?php echo $contratante->cod_poblacion; ?></td> 
-<td> 
- <a href='/base/contratante/edit/<?php echo $contratante->id_contratante; ?>' 
- class='btn btn-default btn-xs' title='Edit'>
-<i class='fa fa-edit'></i>
-</a> 
-<a href='/base/contratante/delete/<?php echo $contratante->id_contratante; ?>' 
- class='btn btn-danger btn-xs' title='Delete'>
-<i class='fa fa-trash'></i></a> 
- </td> 
-</tr> 
-<?php endforeach;?>
-        </tbody>
-      </table>
+        </h1>
+
+    </section> 
+    <section class='content'>
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title text-blue">
+                    <<?php echo $rsContratante->contratante; ?>
+                    <small>aqu&iacute; puedes configurar todo lo referente a tu empresa</small>
+                </h3>
+            </div>
+            <div class="box-body">
+                <div class="row">
+                    <!-- datos de empresa -->
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-primary">
+                            <div class="inner">
+                                <h3> <?php echo $rsContratante->contratante; ?></h3>
+
+                                <p>Datos de empresa</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-tasks"></i>
+                            </div>
+                            <a href="<?php echo __URL__ . '/base/contratante/detail/' . $rsContratante->id_contratante; ?>" class="small-box-footer">
+                                ver <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- ./datos_empresa -->
+                    <!-- usuarios -->
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-primary">
+                            <div class="inner">
+                                <h3>Usuarios</h3>
+
+                                <p>Gesti&oacute;n de usuarios</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-users"></i>
+                            </div>
+                            <a href="<?php echo __URL__ . '/base/contratante/detail/' . $rsContratante->id_contratante; ?>" class="small-box-footer">
+                                ver <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- ./usuarios -->
+                    <!-- mails -->
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-primary">
+                            <div class="inner">
+                                <h3>Email</h3>
+
+                                <p>Cuentas de envios,logs</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <a href="<?php echo __URL__ . '/base/contratante/detail/' . $rsContratante->id_contratante; ?>" class="small-box-footer">
+                                ver <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- ./mails -->
+                    <!-- plantillas -->
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-primary">
+                            <div class="inner">
+                                <h3>Plantillas</h3>
+                                <p>Plantillas de factura/presupuestos/mails</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-sticky-note"></i>
+                            </div>
+                            <a href="<?php echo __URL__ . '/base/contratante/detail/' . $rsContratante->id_contratante; ?>" class="small-box-footer">
+                                ver <i class="fa fa-arrow-circle-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- ./plantillas -->
+                </div>
+            </div>
+        </div>
     </section>
 </div>
