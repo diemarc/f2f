e86a3416185dedac8df34795b63ccb08e860a108
diemarc <diemarc.os@gmail.com>
2018-05-30 06:21:41 +0200

    mail config

--- application/modules/configuracion/view/mailusercontratante/index.php
+++ application/modules/configuracion/view/mailusercontratante/index.php
@@ -2,7 +2,7 @@
     <section class="content-header">
         <h1>
             <i class="fa fa-wrench fa-2x"></i> 
-            <a href="<?php echo __URL__.'/base/contratante/index';?>">
+            <a href="<?php echo __URL__ . '/base/contratante/index'; ?>">
                 Configuraci&oacute;n f2F</a>->Cuentas de mail
 
         </h1>
@@ -11,40 +11,63 @@
     <section class='content'>
         <div class="box">
             <div class="box-header with-border">
-                 <a href="<?php echo __URL__.'/configuracion/mailconfig/add';?>" 
-                    class="btn btn-success btn-sm">
-                     <i class="fa fa-plus"></i>
-                 </a>
                 <h3 class="box-title text-blue">
                     <small>aqu&iacute; puedes configurar todo lo referente a los envios de emails</small>
                 </h3>
-                
+
             </div>
             <div class="box-body">
-                <table class="table table-bordered table-condensed table-hover">
-                    <thead class='bg-primary'>
-                        <tr>
-                            <th>Cuenta</th> 
-                            <th>Email</th> 
-                            <th></th> 
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($rsMailusercontratantes AS $mailconfig): ?>
-                            <tr> 
-                                <td><?php echo $mailconfig->account;?></td>
-                                <td><?php echo $mailconfig->mail_address;?></td>
-                                <td> 
-                                    <a href='/configuracion/mailusercontratante/edit/<?php echo $mailconfig->id_mail_account; ?>' 
-                                       class='btn btn-default btn-xs' title='Mail detail'>
-                                        <i class='fa fa-tasks'></i>
-                                    </a> 
-                                </td> 
-                            </tr> 
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+
+                <div class="box box-info">
+                    <div class="box-header with-border">
+                        <h3 class="box-title text-blue">Cuentas de correo</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" 
+                                    data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="table-responsive">
+                            <table class="table no-margin">
+                                <thead>
+                                    <tr>
+                                        <th>Cuenta</th>
+                                        <th>Email</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($rsMailusercontratantes AS $mailconfig): ?>
+                                        <tr> 
+                                            <td><?php echo $mailconfig->account; ?></td>
+                                            <td><?php echo $mailconfig->mail_address; ?></td>
+                                            <td> 
+                                                <a href='/configuracion/mailconfig/detail/<?php echo $mailconfig->id_mail_account; ?>' 
+                                                   class='btn btn-default btn-xs' title='Mail detail'>
+                                                    <i class='fa fa-tasks'></i>
+                                                </a> 
+                                            </td> 
+                                        </tr> 
+                                    <?php endforeach; ?>
+                                   
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.table-responsive -->
+                    </div>
+                    <!-- /.box-body -->
+                    <div class="box-footer clearfix">
+                        <a href="<?php echo __URL__ . '/configuracion/mailconfig/add'; ?>" 
+                           class="btn btn-sm btn-info btn-flat pull-left">Nueva cuenta</a>
+                    </div>
+                    <!-- /.box-footer -->
+                </div>
+
             </div>
         </div>
     </section>
