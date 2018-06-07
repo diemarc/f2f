5272bc27c5aae9ddc83590fde77a929d47ef158c
diemarc <diemarc.os@gmail.com>
2018-06-07 05:06:23 +0200

    f2f mail helper.

--- application/modules/configuracion/model/tables/MailUserContratanteTable.php
+++ application/modules/configuracion/model/tables/MailUserContratanteTable.php
@@ -66,10 +66,12 @@ abstract class MailUserContratanteTable extends \kerana\Ada
         $this->_query = ' SELECT A.id_mail_account,A.id_user,A.id_contratante,B.account,'
                 . ' B.mail_address,B.mail_username,B.mail_password,B.mail_smtp_server,'
                 . ' B.mail_smtp_auth,B.mail_smtp_port,'
-                . ' CAST(AES_DECRYPT(B.mail_password,:secret)AS CHAR) AS pass_decrypt '
+                . ' CAST(AES_DECRYPT(B.mail_password,:secret)AS CHAR) AS pass_decrypt,'
+                . ' D.username, D.name,D.lastname '
                 . ' FROM user_contratante_mail A '
                 . ' INNER JOIN sys_mail_account B ON (B.id_mail_account = A.id_mail_account) '
-                . ' INNER JOIN user_contratante C ON (C.id_user = A.id_user AND A.id_contratante = C.id_contratante) '
+                . ' INNER JOIN user_contratante C ON (C.id_user = A.id_user AND A.id_contratante = C.id_contratante)'
+                . ' INNER JOIN sys_user D ON (A.id_user = D.id_user) '
                 . ' WHERE A.id_mail_account IS NOT NULL '
                 . ' AND A.id_contratante = :id_contratante ';
         
