2736a693e545bc8e0ab5f849b03b3a2bc60150c2
diemarc <diemarc.os@gmail.com>
2018-06-07 07:01:39 +0200

    insert method set own var to query, to avoid mixed querys problems.

--- application/modules/configuracion/view/mailusercontratante/detail.php
+++ application/modules/configuracion/view/mailusercontratante/detail.php
@@ -41,7 +41,7 @@
                             </ul>
 
                             <a href="<?php echo __URL__.'/configuracion/mailconfig/edit/'.$rsMailConfig->id_mail_account;?>" class="btn btn-info "><b>Editar configuraci&oacute;n</b></a>
-                            <a href="#" class="btn btn-primary "><b>Enviar mail de prueba</b></a>
+                            <a href="<?php echo __URL__.'/configuracion/mail/sendTestMail/'.$rsMailConfig->id_mail_account;?>" class="btn btn-primary "><b>Enviar mail de prueba</b></a>
                         </div>
                         <!-- /.box-body -->
                     </div>
@@ -51,21 +51,21 @@
                     <div class="box box-primary">
                         <div class="box-header">
                             <i class="fa fa-users"></i>
-
                             <h3 class="box-title text-blue">Usuarios asociados</h3>
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body">
                             <!-- See dist/js/pages/dashboard.js to activate the todoList plugin -->
                             <ul class="todo-list">
+                                <?php foreach($rsUsersMails AS $user):?>
                                 <li>
-                                    <span class="text">F-984534, Pescaderia diarte 57€</span>
-                                    <small class="label label-info"><i class="fa fa-clock-o"></i> Enviada</small>
+                                    <span class="text"><?php echo $user->name.' '.$user->lastname;?></span>
+                                    <small class="label label-info"><i class="fa fa-user"></i> <?php echo $user->username;?></small>
                                     <div class="tools">
-                                        <i class="fa fa-edit"></i>
                                         <i class="fa fa-trash-o"></i>
                                     </div>
                                 </li>
+                                <?php endforeach;?>
                             </ul>
                         </div>
                     </div>
