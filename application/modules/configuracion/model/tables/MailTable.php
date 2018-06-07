30a72573810fa857a4f16d320c09a784f21ea9f0
diemarc <diemarc.os@gmail.com>
2018-06-07 05:09:20 +0200

    mail table

---
+++ application/modules/configuracion/model/tables/MailTable.php
@@ -0,0 +1,310 @@
+<?php
+
+/*
+ * This file is part of keranaProject
+ * Copyright (C) 2017-2018  diemarc  diemarc@protonmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace application\modules\configuracion\model\tables;
+
+defined('__APPFOLDER__') OR exit('Direct access to this file is forbidden, siya');
+/*
+  |-----------------------------------------------------------------------------
+  | Class MailTable
+  |-----------------------------------------------------------------------------
+  |
+  | Persistance layer for sys_email
+  | @author kerana,
+  | @date 07-06-2018 05:05:45,
+  |
+ */
+
+abstract class MailTable extends \kerana\Ada
+{
+
+    protected
+    /** @var int(11), $id_email  */
+            $_id_email,
+            /** @var int(11), $id_mail_account  */
+            $_id_mail_account,
+            /** @var int(11), $id_contratante  */
+            $_id_contratante,
+            /** @var int(11), $id_user  */
+            $_id_user,
+            /** @var varchar(45), $subject  */
+            $_subject,
+            /** @var text(), $body  */
+            $_body,
+            /** @var time(tamp), $created_at  */
+            $_created_at,
+            /** @var int(11), $created_by  */
+            $_created_by,
+            /** Master query for mail */
+            $_query_mail;
+    public
+    /** @array data matching attributes with table field */
+            $data_mail;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->table_name = 'sys_email';
+        $this->table_id = 'id_email';
+
+        $this->pks = [
+            'id_email' => $this->_id_email,
+        ];
+
+        $this->_query = ' SELECT A.id_email,A.id_mail_account,A.id_contratante,'
+                . ' A.id_user,A.subject,A.body,A.created_at,A.created_by,B2.account,'
+                . ' B2.mail_address,B2.mail_username,B2.mail_password,B2.mail_smtp_server,'
+                . ' B2.mail_smtp_auth,B2.mail_smtp_port'
+                . ' FROM sys_email A '
+                . ' INNER JOIN user_contratante_mail B ON (B.id_mail_account = A.id_mail_account) '
+                . ' INNER JOIN sys_mail_account B2 ON (B2.id_mail_account = B.id_mail_account) '
+                . ' INNER JOIN user_contratante B3 ON (B3.id_user = B.id_user) '
+                . ' INNER JOIN sys_user B35 ON (B35.id_user = B3.id_user) '
+                . ' WHERE A.id_email IS NOT NULL ';
+    }
+
+    /*
+      |-------------------------------------------------------------------------
+      | SELECT-METHODS
+      |-------------------------------------------------------------------------
+      |
+     */
+
+
+
+    /*
+      |-------------------------------------------------------------------------
+      | INSERT-UPDATE-METHODS
+      |-------------------------------------------------------------------------
+      |
+     */
+
+    /**
+     * -------------------------------------------------------------------------
+     * Insert/update new record into sys_email
+     * -------------------------------------------------------------------------
+     * @return boolean
+     */
+    public function saveMail()
+    {
+
+        $data_insert = [
+            'id_mail_account' => $this->_id_mail_account,
+            'id_contratante' => $this->_id_contratante,
+            'id_user' => $this->_id_user,
+            'subject' => $this->_subject,
+            'body' => $this->_body,
+            'created_at' => $this->_created_at,
+            'created_by' => $this->_created_by,
+        ];
+        return parent::save($data_insert);
+    }
+
+    /*
+      |-------------------------------------------------------------------------
+      | SETTERS
+      |-------------------------------------------------------------------------
+      |
+     */
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Setter for id_email
+     * ------------------------------------------------------------------------- 
+     * @param int $value the id_email value 
+     */
+    public function set_id_email($value = "")
+    {
+        $this->_id_email = \helpers\Validator::valInt('f_id_email', $value, true);
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Setter for id_mail_account
+     * ------------------------------------------------------------------------- 
+     * @param int $value the id_mail_account value 
+     */
+    public function set_id_mail_account($value = "")
+    {
+        $this->_id_mail_account = \helpers\Validator::valInt('f_id_mail_account', $value, true);
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Setter for id_contratante
+     * ------------------------------------------------------------------------- 
+     * @param int $value the id_contratante value 
+     */
+    public function set_id_contratante($value = "")
+    {
+        $this->_id_contratante = \helpers\Validator::valInt('f_id_contratante', $value, true);
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Setter for id_user
+     * ------------------------------------------------------------------------- 
+     * @param int $value the id_user value 
+     */
+    public function set_id_user($value = "")
+    {
+        $this->_id_user = \helpers\Validator::valInt('f_id_user', $value, true);
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Setter for subject
+     * ------------------------------------------------------------------------- 
+     * @param varchar $value the subject value 
+     */
+    public function set_subject($value = "")
+    {
+        $this->_subject = \helpers\Validator::valVarchar('f_subject', $value, true);
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Setter for body
+     * ------------------------------------------------------------------------- 
+     * @param text $value the body value 
+     */
+    public function set_body($value = "")
+    {
+        $this->_body = \helpers\Validator::valText('f_body', $value, true);
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Setter for created_at
+     * ------------------------------------------------------------------------- 
+     * @param time $value the created_at value 
+     */
+    public function set_created_at($value = "")
+    {
+        $this->_created_at = \helpers\Validator::valTime('f_created_at', $value, true);
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Setter for created_by
+     * ------------------------------------------------------------------------- 
+     * @param int $value the created_by value 
+     */
+    public function set_created_by($value = "")
+    {
+        $this->_created_by = \helpers\Validator::valInt('f_created_by', $value, false);
+    }
+
+    /*
+      |-------------------------------------------------------------------------
+      | GETTERS
+      |-------------------------------------------------------------------------
+      |
+     */
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Getter for id_email
+     * ------------------------------------------------------------------------- 
+     * @return int $value  
+     */
+    public function get_id_email()
+    {
+        return (isset($this->_id_email)) ? $this->_id_email : null;
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Getter for id_mail_account
+     * ------------------------------------------------------------------------- 
+     * @return int $value  
+     */
+    public function get_id_mail_account()
+    {
+        return (isset($this->_id_mail_account)) ? $this->_id_mail_account : null;
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Getter for id_contratante
+     * ------------------------------------------------------------------------- 
+     * @return int $value  
+     */
+    public function get_id_contratante()
+    {
+        return (isset($this->_id_contratante)) ? $this->_id_contratante : null;
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Getter for id_user
+     * ------------------------------------------------------------------------- 
+     * @return int $value  
+     */
+    public function get_id_user()
+    {
+        return (isset($this->_id_user)) ? $this->_id_user : null;
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Getter for subject
+     * ------------------------------------------------------------------------- 
+     * @return varchar $value  
+     */
+    public function get_subject()
+    {
+        return (isset($this->_subject)) ? $this->_subject : null;
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Getter for body
+     * ------------------------------------------------------------------------- 
+     * @return text $value  
+     */
+    public function get_body()
+    {
+        return (isset($this->_body)) ? $this->_body : null;
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Getter for created_at
+     * ------------------------------------------------------------------------- 
+     * @return time $value  
+     */
+    public function get_created_at()
+    {
+        return (isset($this->_created_at)) ? $this->_created_at : null;
+    }
+
+    /**
+     * ------------------------------------------------------------------------- 
+     * Getter for created_by
+     * ------------------------------------------------------------------------- 
+     * @return int $value  
+     */
+    public function get_created_by()
+    {
+        return (isset($this->_created_by)) ? $this->_created_by : null;
+    }
+
+}
