cf588f22ffaa199b6bfecf34d2a6b541fabeaa16
diemarc <diemarc.os@gmail.com>
2018-06-08 05:46:08 +0200

    Change function call "asignarEmpresa" to the new function assignClient

--- public/src/js/empresa_js.js
+++ public/src/js/empresa_js.js
@@ -64,6 +64,9 @@ function findEmpresa(empresa) {
  */
 function assignClient(id_empresa) {
 
+
+    alert("es esta empresa "+id_empresa);
+
     var url = '/fac2fast/empresa/getEmpresaJson/' + id_empresa;
 
     $.getJSON(url, function (data) {
